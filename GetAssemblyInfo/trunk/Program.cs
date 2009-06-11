afdb26f7bab60e0279a29376220b29b7aea3bff5
pljones <pljones@f493c2bb-01dc-429b-941c-c5e906e792d3>
2009-06-11 13:23:26 +0000

    Correct long-standing typo in my email address... /sheesh/

--- GetAssemblyInfo/trunk/Program.cs
+++ GetAssemblyInfo/trunk/Program.cs
@@ -1,6 +1,6 @@
 ﻿/***************************************************************************
  *  Copyright (C) 2009 by Peter L Jones                                    *
- *  peter@users.sf.net                                                     *
+ *  pljones@users.sf.net                                                   *
  *                                                                         *
  *  GetAssemblyInfo is free software: you can redistribute it and/or       *
  *  modify it under the terms of the GNU General Public License as         *
