162f88bfba45f537b815c865870d0001bb8c4c6f
pljones <pljones@f493c2bb-01dc-429b-941c-c5e906e792d3>
2010-05-02 20:15:49 +0000

    Don't complain if the working and output folders don't exist when trying to delete them.

--- s3pi Doc/trunk/msbuild.bat
+++ s3pi Doc/trunk/msbuild.bat
@@ -3,8 +3,8 @@ set CopyTo=.\Help
 set OutputPath=C:\Windows\Temp\Help
 set WorkingPath=C:\Windows\Temp\HelpWork
 
-rmdir /s/q %OutputPath%
-rmdir /s/q %WorkingPath%
+if exist "%OutputPath%\" rmdir /s/q %OutputPath%
+if exist "%WorkingPath%\" rmdir /s/q %WorkingPath%
 
 c:\Windows\Microsoft.NET\Framework\v3.5\msbuild s3pi.shfbproj "/p:OutputPath=%OutputPath:\=\\%" "/p:WorkingPath=%WorkingPath:\=\\%"
 if ERRORLEVEL 1 goto FAIL
