17aee8d5c181613c588096a937f4c7d7b6e41df6
pljones <pljones@f493c2bb-01dc-429b-941c-c5e906e792d3>
2011-07-09 18:44:27 +0000

    Split CustomForms out - leave non-s3pi controls in CustomForms, put s3pi-specific controls in s3piControls.

--- s3pi Extras/s3piControls/Properties/AssemblyInfo.cs
+++ s3pi Extras/s3piControls/Properties/AssemblyInfo.cs
@@ -5,15 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CustomForms")]
-[assembly: AssemblyDescription("Assorted Windows Forms components (no s3pi dependencies).")]
+[assembly: AssemblyTitle("s3piControls")]
+[assembly: AssemblyDescription("Assorted Windows Forms components related to s3pi objects.")]
 #if DEBUG
 [assembly: AssemblyConfiguration("[DEBUG]")]
 #else
 [assembly: AssemblyConfiguration("")]
 #endif
 [assembly: AssemblyCompany("Peter L Jones")]
-[assembly: AssemblyProduct("CustomForms")]
+[assembly: AssemblyProduct("s3piControls")]
 [assembly: AssemblyCopyright("Copyright (C) 2011  Peter L Jones.  Released under GPL 3.  See gpl-3.0.txt")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -22,6 +22,9 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f76277f2-50c9-49c6-a803-d0d383bce050")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,4 +36,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("0.1.2.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
