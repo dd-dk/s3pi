17e2ed7600990f7d9618bd321717fd2020df32e5
pljones <pljones@f493c2bb-01dc-429b-941c-c5e906e792d3>
2012-01-02 21:13:53 +0000

    Add static Getter functions to custom attributes.

--- s3pi/Interfaces/DataGridExpandableAttribute.cs
+++ s3pi/Interfaces/DataGridExpandableAttribute.cs
@@ -42,5 +42,23 @@ namespace s3pi.Interfaces
         /// Indicate whether the element should be expandable (true) or not (false)
         /// </summary>
         public bool DataGridExpandable { get { return dataGridExpandable; } set { dataGridExpandable = value; } }
+
+        /// <summary>
+        /// Return the DataGridExpandable value for a Content Field.
+        /// </summary>
+        /// <param name="t">Type on which Content Field exists.</param>
+        /// <param name="index">Content Field name.</param>
+        /// <returns>The value of the DataGridExpandableAttribute DataGridExpandable field, if found;
+        /// otherwise <c>false</c>.</returns>
+        public static bool GetDataGridExpandable(Type t, string index)
+        {
+            System.Reflection.PropertyInfo pi = t.GetProperty(index);
+
+            if (pi != null)
+                foreach (var attr in pi.GetCustomAttributes(typeof(DataGridExpandableAttribute), true))
+                    return (attr as DataGridExpandableAttribute).DataGridExpandable;
+
+            return false;
+        }
     }
 }
