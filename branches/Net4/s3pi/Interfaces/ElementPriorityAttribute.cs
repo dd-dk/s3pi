6bbbb5549740932267b6449b51d91231bee2841e
pljones <pljones@f493c2bb-01dc-429b-941c-c5e906e792d3>
2012-01-02 21:13:53 +0000

    Add static Getter functions to custom attributes.

--- branches/Net4/s3pi/Interfaces/ElementPriorityAttribute.cs
+++ branches/Net4/s3pi/Interfaces/ElementPriorityAttribute.cs
@@ -38,5 +38,23 @@ namespace s3pi.Interfaces
         /// Element priority, lower values are higher priority
         /// </summary>
         public Int32 Priority { get { return priority; } set { priority = value; } }
+
+        /// <summary>
+        /// Return the ElementPriority value for a Content Field.
+        /// </summary>
+        /// <param name="t">Type on which Content Field exists.</param>
+        /// <param name="index">Content Field name.</param>
+        /// <returns>The value of the ElementPriorityAttribute Priority field, if found;
+        /// otherwise Int32.MaxValue.</returns>
+        public static Int32 GetPriority(Type t, string index)
+        {
+            System.Reflection.PropertyInfo pi = t.GetProperty(index);
+
+            if (pi != null)
+                foreach (var attr in pi.GetCustomAttributes(typeof(ElementPriorityAttribute), true))
+                    return (attr as ElementPriorityAttribute).Priority;
+
+            return Int32.MaxValue;
+        }
     }
 }
